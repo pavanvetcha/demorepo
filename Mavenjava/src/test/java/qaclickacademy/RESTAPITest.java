07b899a75ee671775a5747210b69a40288e586bc
pavan <pavanvetcha558@gmail.com>
2022-08-23 12:58:22 +0530

    second commit

---
+++ Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+		
+	}
+	@Test
+	public void deletefacebook()
+	{
+		System.out.println("deletefacebook");
+	}
+
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/SeliniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeliniumTest {
+	@Test
+
+	public void browserautomation() {
+		System.out.println("browserautomation");
+
+	}
+
+	@Test
+	public void elements() {
+		System.out.println("Elements");
+	}
+
+}
